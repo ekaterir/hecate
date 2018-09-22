c0742c0919901108860d3438af007c93bcfbc1ee
Ekaterina Ryabtseva <kate.ryabtseva@gmail.com>
2018-09-22 00:01:51 -0400

    Creating embedding works now.

--- packages/server/index.js
+++ packages/server/index.js
@@ -1,5 +1,7 @@
 const express = require('express')
-const app = express()
+var bodyParser = require('body-parser');
+require('dotenv').load();
+const app = express();
 const port = 8080
 
 app.use(function (req, res, next) {
@@ -17,11 +19,14 @@ app.use(function (req, res, next) {
     next();
 });
 
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.get('/enrollments', (req, res) => {
   const {spawn} = require('child_process');
-  const getEnrollments = spawn('./get_enrolled_speakers.sh', [], {cwd: '/Users/eryabtseva/Projects/gbo-demo/egs/voxceleb/gbo_demo'});
+  const getEnrollments = spawn('./get_enrolled_speakers.sh', [], {cwd: process.env.COMMANDS_EXECUTION_DIRECTORY });
   let result = '';
   getEnrollments.stdout.on('data', (chunk) => {
     result += chunk.toString('utf-8');
@@ -31,8 +36,24 @@ app.get('/enrollments', (req, res) => {
   });
   getEnrollments.on('close', (code) => {
       let response = {};
-      response.enrollments = result;
+      response.enrollments = result.replace(/_/g , " ");
       res.end(JSON.stringify(response));
+  });
+})
+
+app.post('/embeddings', (req, res) => {
+  const {spawn} = require('child_process');
+  const youtubeId = req.body.youtubeId;
+  const createEmbedding = spawn('./create_embedding.sh', [ youtubeId ], {cwd: process.env.COMMANDS_EXECUTION_DIRECTORY });
+  result = '';
+  createEmbedding.stdout.on('data', (chunk) => {
+    result += chunk.toString('utf-8');
+  });
+  createEmbedding.stderr.on('data', (data) => {
+      console.log(`tderr: ${data}`);
+  });
+  createEmbedding.on('close', (code) => {
+      res.end(JSON.stringify(result));
   });
 })
 
--- packages/ui/src/App.js
+++ packages/ui/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import logo from './logo.svg';
+import matchSorter from 'match-sorter';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.min.css';
 import './App.css';
 
 class App extends Component {
@@ -9,7 +11,12 @@ class App extends Component {
     super(props);
     this.state = {
       enrollments: [],
-      doRender: false,
+      originalEnrollments: [],
+      loadingEnrollments: true,
+      youtubeId: '',
+      submittingYoutubeId: false,
+      searchString: '',
+      personName: '',
     };
   }
 
@@ -18,26 +25,99 @@ class App extends Component {
       .then((response) => {
         const { data: { enrollments } } = response;
         let enrollmentsArr = enrollments.split(";");
-        this.setState({ doRender: true, enrollments: enrollmentsArr });
+        this.setState({ enrollments: enrollmentsArr.slice(0, 20), originalEnrollments: enrollmentsArr, loadingEnrollments: false });
       })
       .catch((error) => {
         console.log(error);
       });
   }
 
+  handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  choosePerson = (event) => {
+    this.setState({ personName: event.currentTarget.dataset.id})
+  }
+
+  notify = (msg) => toast(msg);
+
+  createEmbedding = (event) => {
+    event.preventDefault();
+    this.setState({ submittingYoutubeId: true })
+    const { youtubeId } = this.state;
+    axios.post('http://localhost:8080/embeddings', { youtubeId })
+      .then((response) => {
+        if (response.data.includes('DONE')) {
+          var url = new URL(youtubeId);
+          var v = url.searchParams.get("v");
+          this.notify('Created embedding ' + v);
+          this.setState({ submittingYoutubeId: false });
+        } else {
+          this.notify('Something went wrong, try again...');
+          this.setState({ submittingYoutubeId: false });
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+        this.notify('Serious error occurred...');
+        this.setState({ submittingYoutubeId: false });
+      });
+  }
+
+  search = (event) => {
+    let enrollments = this.state.originalEnrollments;
+    this.setState({ searchString: event.target.value },
+      () => {
+        enrollments = matchSorter(enrollments, this.state.searchString);
+        this.setState({ enrollments: enrollments.slice(0, 20) });
+      }
+    );
+  }
+
   render() {
-    const { doRender, enrollments } = this.state;
-    if (!doRender) {
-      return (<div>Loading...</div>);
-    }
+    const { loadingEnrollments, enrollments, youtubeId, searchString, submittingYoutubeId, personName } = this.state;
+
     return (
-      <section>
-        <ul>
+      <main>
+        <ToastContainer
+          position="top-right"
+          autoClose={false}
+          newestOnTop={false}
+        />
+        <section className="youtubeInput">
+          <h1>Create Embedding</h1>
+          <form onSubmit={this.createEmbedding}>
+            <label>
+              Youtube ID: <input type="text" name="youtubeId" value={youtubeId} onChange={this.handleChange} />
+            </label>
+            {submittingYoutubeId ? (
+              <input type="submit" value="Computing ..." />
+            ) : (
+              <input type="submit" value="Submit" />
+            )}
+          </form>
+        </section>
+
+        <section className="enrollments-list" style={{ minHeight: '443px'}}>
+          <h1>Enrolled Speakers { !loadingEnrollments && `(${this.state.originalEnrollments.length})`}</h1>
+          { loadingEnrollments && <div id="loader"></div>}
+          <input type="text" value={searchString} placeholder="Search..." onChange={this.search} />
           {enrollments.length > 0 &&
-            enrollments.map((enrollment) => (<li>{enrollment}</li>))
+            <ul>
+              {enrollments.map((enrollment, idx) => (
+                <li key={idx} onClick={this.choosePerson.bind(this)} data-id={enrollment}>{enrollment}</li>
+              ))}
+            </ul>
           }
-        </ul>
-      </section>
+        </section>
+
+        <section>
+          <h1>Actions</h1>
+          <input type="text" value={personName} name="personName" placeholder="Name" onChange={this.handleChange} />
+        </section>
+
+      </main>
     );
   }
 }
